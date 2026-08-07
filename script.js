4b3e1f200cf8a6c3b4a5582e660f85a62518ec8c
Toni <antonia.toninvitation@gmail.com>
2026-08-07 14:03:27 +0100

    atualizacao do site

--- script.js
+++ script.js
@@ -2,19 +2,15 @@ const menu = document.getElementById("menu");
 const botao = document.getElementById("menu-mobile");
 
 botao.addEventListener("click", () => {
-
     menu.classList.toggle("ativo");
     botao.classList.toggle("aberto");
-
 });
 
 document.querySelectorAll("#menu a").forEach(link => {
 
     link.addEventListener("click", () => {
-
         menu.classList.remove("ativo");
         botao.classList.remove("aberto");
-
     });
 
 });
